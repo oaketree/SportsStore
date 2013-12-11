891b31093916bfa990568c3406a203fc0b719c74
oaketree <oaketree@163.com>
2013-12-11 16:52:11 +0800

    下班

--- SportsStore.WebUI/Global.asax.cs
+++ SportsStore.WebUI/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SportsStore.WebUI.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -19,6 +20,7 @@ namespace SportsStore.WebUI
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            ControllerBuilder.Current.SetControllerFactory(new NinjectControllerFactory());
         }
     }
 }
---
+++ SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ninject;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectControllerFactory:DefaultControllerFactory
+    {
+        private IKernel ninjectKernel;
+        public NinjectControllerFactory()
+        {
+            ninjectKernel = new StandardKernel();
+            AddBindings();
+        }
+        protected override IController GetControllerInstance(System.Web.Routing.RequestContext requestContext, Type controllerType)
+        {
+            return controllerType == null ? null : (IController)ninjectKernel.Get(controllerType);
+        }
+        private void AddBindings()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m=>m.Products).Returns(new List<Product>{
+                new Product
+            }.AsQueryable());
+        }
+    }
+   
+}
